aacdc163d4e20c66281aeba0accf9027091f4c71
Dương Tuấn Anh <he180437duongtuananh@gmail.com>
2026-03-17 21:48:03 +0700

    init project

---
+++ backend/src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Backend is running!');
+});
+
+module.exports = app;
